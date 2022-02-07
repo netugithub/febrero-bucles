20bc4cd4a6c910a3945581e992406f6d1f654d01
netanel <netaneldt@gmail.com>
2022-02-07 09:46:17 -0300

    ejercicio 1 al 6

---
+++ bucles/js/index.js
@@ -0,0 +1,35 @@
+// ejercicio 1
+for( i = 1; i <= 17 ; i++){
+    console.log(i)
+}
+
+//ejercicio 2
+for( i = 7; i <= 12 ; i++){
+    console.log(i)
+}
+
+//ejercicio 3
+var arr = [4,5,734,43,45];
+arr.push(1,120)
+console.log(arr)
+
+
+// ejercicio 4
+var arr = [4,5,734,43,45];
+arr.push(1,29,76,49,32,19,345,21,5000,33)
+console.log(arr)
+
+// ejercicio 5
+var myArray =
+[232,32,1,4,55,4,3,32,3,24,5,5,5,34,2,3,5,5365743,52,34,3,55,33,435,4,6,54,63,45,4,67,56,47,1,
+34,54,32,54,1,78,98,0,9,8,98,76,7,54,2,3,42,456,4,3321,5];
+for(let i = 0; i < myArray.length; i++){
+    console.log(myArray[i])    
+}
+
+//ejercicio 6
+var mySampleArray = [3423,5,4,47889,654,8,867543,23,48,56432,55,23,25,12];
+    for(var i = mySampleArray.length; i>0;   i = i - 1){
+    console.log(mySampleArray[i]);
+}
+
